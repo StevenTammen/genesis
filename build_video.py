9313d6e39dc644d7c70c0ce71d2e272a6b44bc43
Steven Tammen <steven@steventammen.com>
2025-07-14 07:43:42 -0400

    In-progress commit

--- build_video.py
+++ build_video.py
@@ -24,17 +24,62 @@ from lib.transcript import *
 #   2) segments.xlsx has internal timestamps defined for any segment-internal timestamps
 
 current_dir_path = os.getcwd()
-clvaa_path = os.path.dirname(sys.argv[0])
+raw_dir_path = current_dir_path + '/recording/raw'
+raw_recording_segment_names = [f for f in os.listdir(raw_dir_path) if f.endswith('.mp4')]
+is_single_video_segment_case = len(raw_recording_segment_names) == 1
 
-# Build topic transition segments
-generate_topic_transition_slides(current_dir_path, clvaa_path)
-build_topic_transition_segments(current_dir_path)
+# -----------------------------------------
 
-# Calculate timestamps based on post-silence-removal content recording segment boundaries
-calculate_timestamps_and_write_to_excel_and_yt_desc(current_dir_path)
+# Case: more than one video segment
+if(not is_single_video_segment_case):
 
-# Combine all the segments into a single video file
-combine_video_files(current_dir_path)
+    clvaa_path = os.path.dirname(sys.argv[0])
 
-# Add chapter metadata to the single video file
-add_chapters_to_video_file(current_dir_path)
+    # Build topic transition segments
+    generate_topic_transition_slides(current_dir_path, clvaa_path)
+    build_topic_transition_segments(current_dir_path)
+
+    # Calculate timestamps based on post-silence-removal content recording segment boundaries
+    calculate_timestamps_and_write_to_excel_and_yt_desc(current_dir_path)
+
+    # Combine all the segments into a single video file
+    combine_video_files(current_dir_path)
+
+    # Add chapter metadata to the single video file
+    add_chapters_to_video_file(current_dir_path)
+
+# -----------------------------------------
+
+# Case: only one video segment
+else: # is_single_video_segment_case == True
+
+    content_dir_path = current_dir_path.replace("/mnt/c/Dropbox/recordings/", "/mnt/c/R/")
+
+    # Organize raw first. May refactor this later
+    rename_raw_segments_to_be_in_tens(raw_dir_path)
+
+    raw_recording_segment_names = [f for f in os.listdir(raw_dir_path) if f.endswith('.mp4')]
+
+    if(len(raw_recording_segment_names) > 1):
+        raise Exception('Simple videos should only have a single segment. Please check the raw subdirectory.')
+
+    files_to_move = []
+    for segment_name in raw_recording_segment_names:
+        out_file = segment_name.replace(".mp4", "_silence_removed.mp4")
+        subprocess.run(shlex.split(f'auto-editor recording/raw/{segment_name} --margin 0.5s --my_ffmpeg -vcodec libx264 --extras "-crf 30" --output recording/raw/{out_file} --no-open'))
+        files_to_move.append(out_file)
+
+    for file_to_move in files_to_move:
+        src_path = raw_dir_path + '/' + file_to_move
+        dest_path = current_dir_path + '/' + 'video.mp4'
+        os.rename(src_path, dest_path)
+
+    # Write to YouTube description
+    try:
+        content_page_path = content_dir_path + '/' + '_index.md'
+        full_page_content = read_in_file(content_page_path)
+    # Handle discussion pages as well as content pages
+    except FileNotFoundError:
+        content_page_path = content_dir_path + '/' + 'index.md'
+        full_page_content = read_in_file(content_page_path)
+    write_youtube_description_to_file(current_dir_path, content_dir_path, full_page_content)
--- write_back_to_content_file.py
+++ write_back_to_content_file.py
@@ -4,4 +4,14 @@ import os
 from lib.timestamps import *
 
 current_dir_path = os.getcwd()
-calculate_timestamps_and_write_to_content_file(current_dir_path)
+raw_dir_path = current_dir_path + '/recording/raw'
+raw_recording_segment_names = [f for f in os.listdir(raw_dir_path) if f.endswith('.mp4')]
+is_single_video_segment_case = len(raw_recording_segment_names) == 1
+
+# Case: more than one video segment
+if(not is_single_video_segment_case):
+    calculate_timestamps_and_write_to_content_file(current_dir_path)
+
+# Case: only one video segment
+else: # is_single_video_segment_case == True
+    calculate_timestamps_and_write_to_content_file(current_dir_path, True)
