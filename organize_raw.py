db4c3b67b117f8b0182b5b5174b38e29b4ad7987
Steven Tammen <stevenwtammen@gmail.com>
2025-07-24 17:11:16 -0400

    Add TODO

--- organize_raw.py
+++ organize_raw.py
@@ -4,4 +4,6 @@ from lib.organize import *
 
 current_dir_path = os.getcwd()
 raw_dir_path = current_dir_path +'/recording/raw'
+# TODO: make above replacement handle if it is dropbox lowercase not just Dropbox uppercase. Causes runtime exception if you cd to lowercase path not uppercase path in shell
+
 rename_raw_segments_to_be_in_tens(raw_dir_path)
